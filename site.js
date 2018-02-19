921d5ea6d1b55ba69ee7d0c66b91e88de4d2b714
TylerTomasello <ttomasel@hawk.iit.edu>
2018-02-18 20:35:26 -0600

    fix problems that linters found

--- site.js
+++ site.js
@@ -1,4 +1,3 @@
-// Clicking anywhere in the li focuses the child <input>
 $('html').removeClass('nojs').addClass('js');
 
 $('.inputs li').on('click', function() {
@@ -9,8 +8,10 @@ $('#email').on('keyup', function(){
   var email = {
     val: $(this).val(),
     pat: /^[^\s@]+@[^\s@]+$/
-  }
+  };
   var name = $('#name').val();
+  /* tested and had problems */
+  // var birthday = $('#birthday').val();
   if(name !== "" && (email.pat).test(email.val)) {
     $('input[type="submit"]').addClass('show');
   }
